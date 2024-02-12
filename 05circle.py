012851687aff8ad85d07a52c87b04382fb8da90c
NessyMoonlight <159246841+NessyMoonlight@users.noreply.github.com>
2024-02-12 19:08:32 +0700

    Add files via upload

---
+++ 05circle.py
@@ -0,0 +1,10 @@
+import turtle as t
+
+t.begin_fill()
+t.color('blue')
+t.right(90)
+t.circle(100,180)
+
+t.right(270)
+t.forward(200)
+t.end_fill()
---
+++ circle.py
@@ -0,0 +1,7 @@
+import turtle
+
+t = turtle.Pen()
+t.begin_fill()
+t.color('red')
+t.circle(100)
+t.end_fill()
